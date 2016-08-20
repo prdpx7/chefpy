af7d31f2d0995fb5e4bae3c6dc82f9663a66138d
zuck007 <pradeepchoudhary009@gmail.com>
2016-08-20 17:21:26 +0530

    fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="chefpy",
-      version="0.314",
+      version="0.315",
       description="Unofficial Codechef Api for user's status,rank,solutions etc.",
       url="http://github.com/zuck007/chefpy",
       author="Pradeep Khileri",
